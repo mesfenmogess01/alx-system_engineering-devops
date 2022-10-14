c39b65b49968287f8b02be0cc0c4082817da4123
mesfen <mesfen145mogess@gmail.com>
2022-10-14 14:26:47 +0300

    creat file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-#creates a file in the tmp dir of the node
-
-file { 'school':
-	path    => '/tmp/school',
-	mode    => '0744',
-	owner   => 'www-data',
-	group   => 'www-data',
-	content => 'I Love Puppet'
+# file in tmp school
+file { '/tmp/school':
+      ensure  => file,
+	path  => '/tmp/school',
+	mode  => '0744',
+	owner => 'www-data',
+	group => 'www-data',
+      content => 'I love Puppet'
 }
