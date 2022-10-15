3346431f05c5e438e468ec24c259fdf5cc64a065
mesfen <mesfen145mogess@gmail.com>
2022-10-15 10:14:50 +0300

    creat file

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # kills a process
 exec { 'killmenow':
-     command => 'pkill -f killmenow',
-     path    => '/usr/bin:/usr/sbin:/bin'
+     command  => 'pkill -f killmenow',
+     path     => '/usr/bin:/usr/sbin:/bin'
 }
